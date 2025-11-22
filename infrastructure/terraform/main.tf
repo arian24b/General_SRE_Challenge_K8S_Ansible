29df74f8eae99c19e44d4146e393483e98554e4b
Arian Omrani <arianomrani24@gmail.com>
2025-11-22 19:45:55 +0330

    fix: Refactor S3 backend configuration in Terraform to use proper syntax for endpoints

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -11,11 +11,14 @@ terraform {
     key    = "k8s-cluster/terraform.tfstate"
     region = "default"
 
-    endpoints.s3                    = "https://s3.ir-thr-at1.arvanstorage.ir"
+    endpoints = {
+      s3 = "https://s3.ir-thr-at1.arvanstorage.ir"
+    }
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
     use_path_style              = true
   }
 }
