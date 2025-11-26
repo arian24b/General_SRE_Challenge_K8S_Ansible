847de228eaf80d6681af349fe151cf1c0d5d759f
Arian Omrani <arianomrani24@gmail.com>
2025-11-26 12:09:15 +0330

    fix: Add backend configuration for S3 in Terraform

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -6,6 +6,21 @@ terraform {
     }
   }
 
+    backend "s3" {
+    bucket = "sre-challenge-terraform"
+    key    = "k8s-cluster/terraform.tfstate"
+    region = "main"
+
+    endpoints = {
+      s3 = "https://s3.ir-thr-at1.arvanstorage.ir"
+    }
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+  }
 }
 
 provider "arvan" {
